d788e82aaeee69025349acc4a1dca20db7aa2566
Junha0303 <qkrwnsgk0303@users.noreply.github.com>
2021-07-05 15:01:59 +0900

    Initial Commit

---
+++ RGB/RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBApp.swift
+//  RGB
+//
+//  Created by 박준하 on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
